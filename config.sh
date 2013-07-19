3927c02b405000fcc096841bfcbae7bc8192ba64
Aaron Chen <nextAaron@gmail.com>
2013-07-19 16:13:18 -0700

    configure batch

--- config.sh
+++ config.sh
@@ -1,1 +1,2 @@
-CFLAGS="-O2 -I/opt/ported/include/ncurses" LIBS=-L/opt/ported/lib LIBCURL_CFLAGS=-I/opt/ported/include LIBCURL_LIBS="-lcurl -L/opt/ported/lib" LIBUSB_CFLAGS=-I/opt/local/include LIBUSB_LIBS="-lusb-1.0 -L/opt/local/lib" ./configure --disable-opencl --enable-icarus
+CFLAGS="-O2 -I/opt/ported/include/ncurses -I/opt/local/include" LIBS="-L/opt/ported/lib -L/opt/local/lib" LIBCURL_CFLAGS=-I/opt/ported/include LIBCURL_LIBS="-lcurl -L/opt/ported/lib" LIBUSB_CFLAGS=-I/opt/local/include LIBUSB_LIBS="-lusb-1.0 -L/opt/local/lib" ./configure --enable-scrypt --enable-icarus
+#--disable-opencl --enable-icarus
